0651c7e1d636bfb23abb25286317b9abf585dbef
am230 <111672334+am230@users.noreply.github.com>
2023-11-10 16:04:55 +0900

    MessageComponentRenderer

---
+++ src/lib/common/omuchat/MessageComponentRenderer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { MessageComponentType } from '@/omuchat';
+
+	export let component: MessageComponentType;
+</script>
+
+{#if component.type === 'text'}
+	{component.value || ''}
+{:else if component.type === 'image'}
+	<img src={component.value} alt={component.id} />
+{/if}
+{#if component.siblings}
+	{#each component.siblings as sibling}
+		<svelte:self component={sibling} />
+	{/each}
+{/if}
---
+++ src/lib/common/omuchat/MessageRenderer.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import type { Message } from '@/omuchat';
+	import MessageComponentRenderer from './MessageComponentRenderer.svelte';
+
+	export let message: Message;
+</script>
+
+<div class="message">
+	<div class="left">
+		{#if message.author && message.author.image_url}
+			<img src={message.author.image_url} alt="avatar" class="author-avatar" />
+		{/if}
+	</div>
+	<div class="right">
+		{#if message.author}
+			<div class="author-name">{message.author.name}</div>
+		{/if}
+		{#if message.content}
+			<div class="message-content">
+				<MessageComponentRenderer component={message.content} />
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.message {
+		display: flex;
+		flex-direction: row;
+		gap: 10px;
+		margin-left: 10px;
+	}
+
+	.left {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 5px;
+	}
+
+	.author-avatar {
+		width: 24px;
+		height: 24px;
+		border-radius: 50%;
+	}
+
+	.author-name {
+		font-size: 0.8rem;
+		opacity: 0.5;
+	}
+
+	.message-content {
+		font-size: 1rem;
+
+		& > img {
+			max-height: 30px;
+			object-fit: contain;
+		}
+
+		& > * {
+			margin-top: 5px;
+		}
+	}
+</style>
