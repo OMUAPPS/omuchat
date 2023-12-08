3a49c27694e3ac3d90a4f62c75f12066343b7642
am230 <111672334+am230@users.noreply.github.com>
2023-12-08 13:52:49 +0900

    スクリプトをcjsからmjsに変更

--- script/tabler-icons.cjs
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const fse = require('fs-extra');
-
-
-const srcDir = path.join(__dirname, '..', 'node_modules', '@tabler', 'icons-webfont');
-const destDir = path.join(__dirname, '..', 'static', 'icons-webfont');
-fse.copySync(srcDir, destDir, { overwrite: true })
