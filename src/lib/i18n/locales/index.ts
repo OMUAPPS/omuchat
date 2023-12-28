c664d7c41777d19efbd30ed8266e6d30b1b83162
am230 <111672334+am230@users.noreply.github.com>
2023-12-28 22:13:22 +0900

    一時的に言語を日本語に限定

--- src/lib/i18n/locales/index.ts
+++ src/lib/i18n/locales/index.ts
@@ -14,29 +14,5 @@ export const LOCALES = {
     'ja-JP': {
         name: '日本語',
         load: () => loadI18n(() => import('./ja-JP.json'), 'ja-JP')
-    },
-    'ja-JP-hiragana': {
-        name: 'ひらがな',
-        load: () => loadI18n(() => import('./ja-JP-hiragana.json'), 'ja-JP-hiragana')
-    },
-    'ja-PD': {
-        name: 'Pseudo Japanese',
-        load: () => loadI18n(() => import('./ja-PD.json'), 'ja-PD')
-    },
-    'ja-EN': {
-        name: 'English Japanese',
-        load: () => loadI18n(() => import('./ja-EN.json'), 'ja-EN')
-    },
-    'en-US': {
-        name: 'English (US)',
-        load: () => loadI18n(() => import('./en-US.json'), 'en-US')
-    },
-    'ar-EG': {
-        name: 'العربية',
-        load: () => loadI18n(() => import('./ar-EG.json'), 'ar-EG')
-    },
-    'zh-CN': {
-        name: '简体中文',
-        load: () => loadI18n(() => import('./zh-CN.json'), 'zh-CN')
-    },
+    }
 };
