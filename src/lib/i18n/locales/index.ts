eba6343afa96107482e4f282cce9724a30126682
am230 <111672334+am230@users.noreply.github.com>
2024-01-19 16:27:31 +0900

    chore: 言語ファイルの型をより明示的に

--- src/lib/i18n/locales/index.ts
+++ src/lib/i18n/locales/index.ts
@@ -1,11 +1,11 @@
-import { createI18n, type I18n } from '../i18n';
+import { createI18n, type I18n, type Messages } from '../i18n.js';
 
 export interface Entry {
     name: string;
     load: () => Promise<I18n>;
 }
 
-async function loadI18n(load: () => any, locale: string): Promise<I18n> {
+async function loadI18n(load: () => Promise<{ default: Messages }>, locale: string): Promise<I18n> {
     const { default: messages } = await load();
     return createI18n(messages, locale);
 }
