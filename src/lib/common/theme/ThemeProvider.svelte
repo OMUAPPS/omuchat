b23dd2f5e601888e31ba87ce54e879c574382bc2
am230 <111672334+am230@users.noreply.github.com>
2024-02-02 17:06:48 +0900

    perf: svelte:elementを使ったテーマcssの埋め込み

--- src/lib/common/theme/ThemeProvider.svelte
+++ src/lib/common/theme/ThemeProvider.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
     import { theme } from "./theme.js";
-
-    let styleDiv: HTMLElement;
-
-    onMount(() => {
-        theme.subscribe((theme) => {
-            styleDiv.innerHTML = '';
-            const style = document.createElement('style');
-            styleDiv.appendChild(style);
-            const css = Object.entries(theme).map(([key, value]) => `${key}: ${value};`).join('\n');
-            style.innerHTML = `
-                :root {
-                    ${css}
-                }
-            `;
-        });
-    });
 </script>
 
-<div bind:this={styleDiv} class="theme-provider" />
+<svelte:head>
+    <svelte:element this="style">
+        :root {'{'}
+            {Object.entries($theme).map(([key, value]) => `${key}: ${value};`).join('\n')}
+        {'}'}
+    </svelte:element>
+</svelte:head>
