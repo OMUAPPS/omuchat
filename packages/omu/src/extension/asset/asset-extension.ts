e9449879d92c6a513983fdb1ef94ee48f570dad9
am230 <111672334+am230@users.noreply.github.com>
2024-02-13 20:28:57 +0900

    enhance(omu): AssetExtensionをバイトデータに対応させた

--- packages/omu/src/extension/asset/asset-extension.ts
+++ packages/omu/src/extension/asset/asset-extension.ts
@@ -1,43 +1,47 @@
+import { Serializer } from '../../interface/serializable.js';
 import type { Client } from '../../client/index.js';
-import { JsonEndpointType } from '../endpoint/endpoint.js';
+import { SerializeEndpointType } from '../endpoint/endpoint.js';
 import { defineExtensionType } from '../extension.js';
+import { ByteReader, ByteWriter } from '../../helper.js';
 
 export const AssetExtensionType = defineExtensionType('asset', {
     create: (client: Client) => new AssetExtension(client),
 });
 
-interface PutEvent {
-    [key: string]: string;
-}
+type Files = Array<{ key: string; buffer: Uint8Array }>;
 
-export const AssetUploadEndpoint = JsonEndpointType.ofExtension<PutEvent, string[]>(AssetExtensionType, {
+const FILES_SERIALIZER = new Serializer<Files, Uint8Array>((files) => {
+    const writer = new ByteWriter();
+    writer.writeInt(files.length);
+    for (const file of files) {
+        writer.writeString(file.key);
+        writer.writeByteArray(file.buffer);
+    }
+    return writer.finish();
+}, (data) => {
+    const reader = new ByteReader(data);
+    const count = reader.readInt();
+    const files: Files = [];
+    for (let i = 0; i < count; i++) {
+        const key = reader.readString();
+        const buffer = reader.readByteArray();
+        files.push({ key, buffer });
+    }
+    reader.finish();
+    return files;
+});
+
+export const AssetUploadEndpoint = SerializeEndpointType.ofExtension<Files, string[]>(AssetExtensionType, {
     name: 'upload',
+    requestSerializer: FILES_SERIALIZER,
+    responseSerializer: Serializer.json(),
 });
 
 export class AssetExtension {
     constructor(private readonly client: Client) {
     }
 
-    async upload(...files: Array<{ key: string; buffer: ArrayBuffer }>): Promise<string[]> {
-        return this.client.endpoints.call(AssetUploadEndpoint, Object.fromEntries(await Promise.all(files.map(async (file) => [file.key, await this.encode(file.buffer)]))));
-    }
-
-    // TODO: バイト列を直接渡せるようにイベントをバイナリで送るようにする
-    private async encode(array: ArrayBuffer): Promise<string> {
-        // if (typeof Buffer !== 'undefined') {
-        //     return Buffer.from(array).toString('base64');
-        // }
-        // fast base64 encode method: https://gist.github.com/fonsp/a4a2d127f35e72deec14227c1bdf3d04#file-measure_performance-js
-        const base64url = await new Promise<string>((resolve) => {
-            const reader = new FileReader();
-            reader.onload = (): void => {
-                if (typeof reader.result !== 'string') {
-                    throw new Error('Invalid result');
-                }
-                return resolve(reader.result);
-            };
-            reader.readAsDataURL(new Blob([array]));
-        });
-        return base64url.split(',', 2)[1];
+    async upload(...files: Files): Promise<string[]> {
+        return this.client.endpoints.call(AssetUploadEndpoint, files);
     }
 }
