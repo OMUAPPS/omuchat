840ddd96b987887c3b53012628ac21aba325ddc2
am230 <111672334+am230@users.noreply.github.com>
2024-02-28 00:12:33 +0900

    Update PanelProvider.svelte

--- src/lib/main/panel/PanelProvider.svelte
+++ src/lib/main/panel/PanelProvider.svelte
@@ -1,65 +1,67 @@
 <script lang="ts">
-    import { get, writable, type Writable } from 'svelte/store';
+	import { get, writable, type Writable } from 'svelte/store';
 
-    import { setPanelContext, type PanelContext, type PanelEntry } from './panel.js';
-    import Panel from './Panel.svelte';
+	import { setPanelContext, type PanelContext, type PanelEntry } from './panel.js';
+	import Panel from './Panel.svelte';
 
-    export let panels: Writable<PanelEntry[]> = writable([]);
+	export let panels: Writable<PanelEntry[]> = writable([]);
 
-    const selected = writable<PanelEntry | null>(null);
-    const context: PanelContext = {
-        panels,
-        addPanel(panel: PanelEntry) {
-            panels.update((panels) => [...panels, panel]);
-        },
-        removePanel(panel: PanelEntry) {
-            panels.update((panels) => panels.filter((p) => p !== panel));
-        },
-        swapPanel(from: PanelEntry, to: PanelEntry) {
-            if (!from.index) return;
-            if (!to.index) return;
-            const temp = get(from.index);
-            from.index.set(get(to.index));
-            to.index.set(temp);
-        },
-        updateDrag(panel: PanelEntry) {
-            const rectFrom = panel.element!.getBoundingClientRect();
-            for (const p of $panels) {
-                if (p === panel) continue;
-                const rectTo = p.element!.getBoundingClientRect();
-                if (rectFrom.right > rectTo.left && rectFrom.left < rectTo.right) {
-                    $selected = p;
-                    return;
-                }
-            }
-            $selected = null;
-        },
-        dragPanel(panel: PanelEntry) {
-            if (!$selected) return;
-            this.swapPanel(panel, $selected);
-            $selected = null;
-        }
-    };
-    setPanelContext(context);
+	const selected = writable<PanelEntry | null>(null);
+	const context: PanelContext = {
+		panels,
+		addPanel(panel: PanelEntry) {
+			panels.update((panels) => [...panels, panel]);
+		},
+		removePanel(panel: PanelEntry) {
+			panels.update((panels) => panels.filter((p) => p !== panel));
+		},
+		swapPanel(from: PanelEntry, to: PanelEntry) {
+			if (!from.index) return;
+			if (!to.index) return;
+			const temp = get(from.index);
+			from.index.set(get(to.index));
+			to.index.set(temp);
+		},
+		updateDrag(panel: PanelEntry) {
+			if (panel.element === undefined) return;
+			const rectFrom = panel.element.getBoundingClientRect();
+			for (const other of $panels) {
+				if (other === panel) continue;
+				if (other.element === undefined) return;
+				const rectTo = other.element.getBoundingClientRect();
+				if (rectFrom.right > rectTo.left && rectFrom.left < rectTo.right) {
+					$selected = other;
+					return;
+				}
+			}
+			$selected = null;
+		},
+		dragPanel(panel: PanelEntry) {
+			if (!$selected) return;
+			this.swapPanel(panel, $selected);
+			$selected = null;
+		}
+	};
+	setPanelContext(context);
 
-    $: $panels.forEach((p, i) => (p.index = writable(i)));
+	$: $panels.forEach((p, i) => (p.index = writable(i)));
 </script>
 
 <div class="container">
-    {#each $panels as panel (panel.name)}
-        <Panel {panel} selected={$selected === panel} />
-    {/each}
+	{#each $panels as panel (panel.name)}
+		<Panel {panel} selected={$selected === panel} />
+	{/each}
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: row;
-        gap: 10px;
-        width: 100%;
-        height: 100%;
-        padding: 5px 10px 10px;
-        overflow-x: auto;
-        background: var(--color-bg-1);
-    }
+	.container {
+		display: flex;
+		flex-direction: row;
+		gap: 10px;
+		width: 100%;
+		height: 100%;
+		padding: 5px 10px 10px;
+		overflow-x: auto;
+		background: var(--color-bg-1);
+	}
 </style>
