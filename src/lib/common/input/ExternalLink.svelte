6b85b2cac810e201d1a0898362877fa68454f203
am230 <111672334+am230@users.noreply.github.com>
2024-03-05 14:20:41 +0900

    enhance(ui): markdown内のリンクをブラウザで開くように

--- src/lib/common/input/ExternalLink.svelte
+++ src/lib/common/input/ExternalLink.svelte
@@ -1,28 +1,10 @@
 <script lang="ts">
-	export let href = '';
-	export let filled = false;
+	export let href: string = '';
+	export let title: string | undefined = undefined;
 
 	if (href?.length && !href.startsWith('http')) {
 		href = `https://${href}`;
 	}
 </script>
 
-{#if href}
-	<a {href} target="_blank" class:filled>
-		{href}
-		<slot />
-	</a>
-{/if}
-
-<style lang="scss">
-	a {
-		font-size: 12px;
-		color: var(--color-1);
-
-		&.filled {
-			color: var(--color-bg-1);
-			text-decoration: none;
-			background: var(--color-1);
-		}
-	}
-</style>
+<a target="_blank" {href} {title}><slot /></a>
