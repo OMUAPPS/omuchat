798cc70f5cd4fab688b78a483562c3a080a28438
am230 <111672334+am230@users.noreply.github.com>
2024-03-15 10:32:05 +0900

    Revert "fix: monorepoパッケージ内でエイリアスでインポート出来ない問題を修正"
    This reverts commit ebddb70b72c431fae9ad73df28cba80605197259.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { searchForWorkspaceRoot } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [tsconfigPaths(), sveltekit()],
+    plugins: [sveltekit()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
     },
