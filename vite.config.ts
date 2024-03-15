ebddb70b72c431fae9ad73df28cba80605197259
am230 <111672334+am230@users.noreply.github.com>
2024-03-15 09:16:17 +0900

    fix: monorepoパッケージ内でエイリアスでインポート出来ない問題を修正
    vite-tsconfig-paths入れてみた

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { searchForWorkspaceRoot } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [tsconfigPaths(), sveltekit()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
     },
