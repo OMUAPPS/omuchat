12076922c8923456670772fa7fe5ad02df45d050
am230 <111672334+am230@users.noreply.github.com>
2024-03-16 09:53:52 +0900

    fix(dashboard): top levelのawaitがビルド通らない問題を修正

--- src/lib/main/page/assets/assets-page.ts
+++ src/lib/main/page/assets/assets-page.ts
@@ -1,15 +1,19 @@
 import { writable } from 'svelte/store';
 
-import { invoke, isOnTauri } from '$lib/utils/tauri.js';
 import { browser } from '$app/environment';
+import { invoke, isOnTauri } from '$lib/utils/tauri.js';
 
 export const origin = writable<string>();
 
-if (browser) {
+async function setOrigin() {
     if (isOnTauri) {
         const res = await invoke('share_url');
         origin.set(`http://${res.host}:${res.port}`);
     } else {
         origin.set(window.location.origin);
     }
+}
+
+if (browser) {
+    setOrigin();
 }
