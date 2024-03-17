1db5eb5177ac2daa529181b7fbbe3e0d67a11c91
am230 <111672334+am230@users.noreply.github.com>
2024-03-17 14:06:26 +0900

    feat(omu): asset-extensionを強化
    * Identifierを使うように変更
    * download関数を追加
    * urlを取得する関数を追加

--- packages/omu/src/extension/asset/asset-extension.ts
+++ packages/omu/src/extension/asset/asset-extension.ts
@@ -1,4 +1,5 @@
 import type { Client } from '../../client/index.js';
+import { Identifier } from '../../identifier.js';
 import { ByteReader, ByteWriter } from '../../network/bytebuffer.js';
 import { Serializer } from '../../serializer.js';
 import { EndpointType } from '../endpoint/endpoint.js';
@@ -9,14 +10,14 @@ export const AssetExtensionType = new ExtensionType(
     (client: Client) => new AssetExtension(client),
 );
 
-type Files = Array<{ key: string; buffer: Uint8Array }>;
+type Files = Array<{ identifier: Identifier; buffer: Uint8Array }>;
 
 const FILES_SERIALIZER = new Serializer<Files, Uint8Array>(
     (files) => {
         const writer = new ByteWriter();
         writer.writeInt(files.length);
         for (const file of files) {
-            writer.writeString(file.key);
+            writer.writeString(file.identifier.key());
             writer.writeByteArray(file.buffer);
         }
         return writer.finish();
@@ -26,28 +27,51 @@ const FILES_SERIALIZER = new Serializer<Files, Uint8Array>(
         const count = reader.readInt();
         const files: Files = [];
         for (let i = 0; i < count; i++) {
-            const key = reader.readString();
+            const identifier = Identifier.fromKey(reader.readString());
             const buffer = reader.readByteArray();
-            files.push({ key, buffer });
+            files.push({ identifier, buffer });
         }
         reader.finish();
         return files;
     },
 );
 
-export const AssetUploadEndpoint = EndpointType.createSerialized<Files, string[]>(
+const IDENTIFIER_SERIALIZER = new Serializer<Identifier, string>(
+    (identifier) => identifier.key(),
+    (key) => Identifier.fromKey(key),
+).array().pipe(Serializer.json());
+
+export const AssetUploadEndpoint = EndpointType.createSerialized<Files, Identifier[]>(
     AssetExtensionType,
     {
         name: 'upload',
         requestSerializer: FILES_SERIALIZER,
-        responseSerializer: Serializer.json(),
+        responseSerializer: IDENTIFIER_SERIALIZER,
+    },
+);
+export const AssetDownloadEndpoint = EndpointType.createSerialized<Identifier[], Files>(
+    AssetExtensionType,
+    {
+        name: 'download',
+        requestSerializer: IDENTIFIER_SERIALIZER,
+        responseSerializer: FILES_SERIALIZER,
     },
 );
 
 export class AssetExtension {
     constructor(private readonly client: Client) { }
 
-    async upload(...files: Files): Promise<string[]> {
+    async upload(...files: Files): Promise<Identifier[]> {
         return this.client.endpoints.call(AssetUploadEndpoint, files);
+    }
+
+    async download(...identifiers: Identifier[]): Promise<Files> {
+        return this.client.endpoints.call(AssetDownloadEndpoint, identifiers);
+    }
+
+    url(identifier: Identifier): string {
+        const address = this.client.network.address;
+        const protocol = address.secure ? 'https' : 'http';
+        return `${protocol}://${address.host}:${address.port}/asset/${identifier.key()}`;
     }
 }
