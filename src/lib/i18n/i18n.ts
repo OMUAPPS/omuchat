e5d339b3a972ac5f932b812bbd7a32a8d190f108
貴雪❆ <71214511+javascriptjp@users.noreply.github.com>
2024-03-17 20:39:29 +0900

    clean: i18n.tsとindex.tsを統合

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -52,3 +52,17 @@ export function createI18nUnion(i18ns: I18n[]): I18n {
         },
     };
 }
+
+async function loadI18n(load: () => Promise<{ default: Messages }>, locale: string): Promise<I18n> {
+    const { default: messages } = await load();
+    return createI18n(messages, locale);
+}
+
+export const LOCALES = {
+    'ja-JP': {
+        name: '日本語',
+        load: () => loadI18n(() => import('./locales/ja-JP.json'), 'ja-JP'),
+    },
+};
+
+export const DEFAULT_LOCALE = 'ja-JP';
