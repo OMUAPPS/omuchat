4e4ea24fb90aeb31c11c75f65b63124784b79f92
am230 <111672334+am230@users.noreply.github.com>
2024-03-19 15:51:46 +0900

    refactor(chat): モデルのjsonの型情報をexportしないように変更

--- packages/chat/src/models/index.ts
+++ packages/chat/src/models/index.ts
@@ -1,9 +1,10 @@
-export { Author, type AuthorJson } from './author.js';
-export { Channel, type ChannelJson } from './channel.js';
+export { Author } from './author.js';
+export { Channel } from './channel.js';
 export * as content from './content.js';
-export { Gift, type GiftJson } from './gift.js';
-export { Message, type MessageJson } from './message.js';
-export { Paid, type PaidJson } from './paid.js';
-export { Provider, type ProviderJson } from './provider.js';
-export { Role, type RoleJson } from './role.js';
-export { Room, type RoomJson } from './room.js';
+export { Gift } from './gift.js';
+export { Message } from './message.js';
+export { Paid } from './paid.js';
+export { Provider } from './provider.js';
+export { Role } from './role.js';
+export { Room } from './room.js';
+
