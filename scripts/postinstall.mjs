dfd290fbd5fbff55f74b7374a27359b6c9a9ce51
am230 <111672334+am230@users.noreply.github.com>
2024-03-22 08:19:02 +0900

    fix(scripts): postinstall時にi18nもビルドするように

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,4 +1,5 @@
 import { execaSync } from 'execa';
 
+execaSync('pnpm', ['--filter', 'i18n', 'ts'], { stderr: process.stderr, stdout: process.stdout });
 execaSync('pnpm', ['--filter', 'omu', 'ts'], { stderr: process.stderr, stdout: process.stdout });
 execaSync('pnpm', ['--filter', 'chat', 'ts'], { stderr: process.stderr, stdout: process.stdout });
