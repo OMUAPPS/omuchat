c2edd990e0bb1ceb30c802daf4243247cbb5ee1b
am230 <111672334+am230@users.noreply.github.com>
2024-03-21 13:13:12 +0900

    refactor(ui): Tooltipを移行 pt.2

--- packages/dash/src/lib/common/titlebar/ButtonMinimize.svelte
+++ packages/dash/src/lib/common/titlebar/ButtonMinimize.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import Button from '../input/Button.svelte';
-    import Tooltip from '../tooltip/Tooltip.svelte';
 
     import { t } from '$lib/i18n/i18n-context.js';
     import { tauriWindow } from '$lib/utils/tauri.js';
+    import { Tooltip } from '@omuchatjs/ui';
 
     function minimize() {
         tauriWindow.appWindow.minimize();
