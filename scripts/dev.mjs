fc296232d5a34abdc8022c42531a10fd806308de
am230 <111672334+am230@users.noreply.github.com>
2024-03-21 13:11:02 +0900

    feat: @omuchatjs/uiにwatchモードを追加

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -2,6 +2,7 @@ import { execa } from 'execa';
 import.meta.url;
 
 execa('pnpm', ['--filter', 'ui', 'storybook'], { stderr: process.stderr, stdout: process.stdout });
+execa('pnpm', ['--filter', 'ui', 'watch'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'omu', 'dev'], { stderr: process.stderr, stdout: process.stdout });
