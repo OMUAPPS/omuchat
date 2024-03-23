b2ba911b92ef6353deaf16eafd93a04af9c472a7
am230 <111672334+am230@users.noreply.github.com>
2024-03-23 19:19:16 +0900

    feat(scripts): nodemonを使ってsrcディレクトリに変更があるたびにビルドする

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -2,7 +2,8 @@ import { execa } from 'execa';
 
 execa('pnpm', ['--filter', 'ui', 'storybook'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'ui', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('pnpm', ['--filter', 'i18n', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('pnpm', ['--filter', 'omu', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('pnpm', ['--filter', 'chat', 'watch'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'omu', 'dev'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'chat', 'dev'], { stderr: process.stderr, stdout: process.stdout });
