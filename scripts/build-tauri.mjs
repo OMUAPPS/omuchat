28907a6adcdc4c38a983019211960c103108dcde
am230 <111672334+am230@users.noreply.github.com>
2024-03-25 09:34:14 +0900

    chore(deps): viteを5.2.6に上げ、他の依存関係もバージョンを上げた

--- scripts/build-tauri.mjs
+++ scripts/build-tauri.mjs
@@ -1,5 +1,25 @@
 import { execa } from 'execa';
 
+execaSync('pnpm', ['--filter', 'i18n', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+
+execaSync('pnpm', ['--filter', 'omu', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+
+execaSync('pnpm', ['--filter', 'chat', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+
+execaSync('pnpm', ['--filter', 'ui', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+
 execa('pnpm', ['--filter', 'dash', 'build'], {
     stderr: process.stderr,
     stdout: process.stdout,
--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -1,4 +1,21 @@
-import { execa } from 'execa';
+import { execa, execaSync } from 'execa';
+
+execaSync('pnpm', ['--filter', 'ui', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+execaSync('pnpm', ['--filter', 'i18n', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+execaSync('pnpm', ['--filter', 'omu', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+execaSync('pnpm', ['--filter', 'chat', 'build'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
 
 execa('pnpm', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
