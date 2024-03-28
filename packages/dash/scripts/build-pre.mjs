6e48ec234ddb4fb1c952ab54021596ef30c73964
am230 <111672334+am230@users.noreply.github.com>
2024-03-28 10:34:10 +0900

    feat(dash): バージョン情報を表示するように

--- packages/dash/scripts/build-pre.mjs
+++ packages/dash/scripts/build-pre.mjs
@@ -40,10 +40,12 @@ async function generateLicense() {
 async function generateVersion() {
     const rootWorkspace = path.resolve(process.cwd(), '..', '..');
     const lerna = JSON.parse(await fs.readFile(path.join(rootWorkspace, 'lerna.json'), 'utf8'));
-    const destFile = path.join('src-tauri', 'tauri.conf.json');
-    const tauriConfig = JSON.parse(await fs.readFile(destFile, 'utf8'));
+    const tauriConfigFile = path.join('src-tauri', 'tauri.conf.json');
+    const tauriConfig = JSON.parse(await fs.readFile(tauriConfigFile, 'utf8'));
     tauriConfig.package.version = lerna.version;
-    await fs.writeFile(destFile, JSON.stringify(tauriConfig, null, 4));
+    await fs.writeFile(tauriConfigFile, JSON.stringify(tauriConfig, null, 4));
+    const versionFile = path.join('src', 'lib', 'version.json');
+    await fs.writeFile(versionFile, JSON.stringify({ version: lerna.version }));
 }
 
 async function main() {
--- packages/dash/src/lib/main/settings/about/Version.svelte
+++ packages/dash/src/lib/main/settings/about/Version.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { t } from '$lib/i18n/i18n-context.js';
+    import { version } from '$lib/version.json';
     import { FlexColWrapper } from '@omuchatjs/ui';
+    import { DEV } from 'esm-env';
 </script>
 
 <FlexColWrapper gap>
@@ -8,5 +10,5 @@
         <i class="ti ti-letter-v" />
         {$t('settings.about.version')}
     </h2>
-    開発中のバージョンです。
+    {version}{DEV ? ' (dev)' : ''}
 </FlexColWrapper>
