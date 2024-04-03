b6da0b0b86cff3c70821fa96fca17f953df3507d
am230 <111672334+am230@users.noreply.github.com>
2024-04-03 19:15:00 +0900

    Update assets-page.ts

--- packages/dash/src/lib/main/page/assets/assets-page.ts
+++ packages/dash/src/lib/main/page/assets/assets-page.ts
@@ -1,19 +1,4 @@
 import { writable } from 'svelte/store';
 
-import { browser } from '$app/environment';
-import { invoke, IS_TAURI } from '$lib/utils/tauri.js';
 
 export const origin = writable<string>();
-
-async function setOrigin() {
-    if (IS_TAURI) {
-        const res = await invoke('share_url');
-        origin.set(`http://${res.host}:${res.port}`);
-    } else {
-        origin.set(window.location.origin);
-    }
-}
-
-if (browser) {
-    setOrigin();
-}
