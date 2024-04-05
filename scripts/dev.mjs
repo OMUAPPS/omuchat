54c6d483105427ff308e01bb8a370f66947eb615
am230 <111672334+am230@users.noreply.github.com>
2024-04-05 21:08:35 +0900

    chore(scripts): devスクリプトでsvelte-checkを実行するようにした

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -19,6 +19,14 @@ execaSync('pnpm', ['--filter', 'chat', 'build'], {
 
 execa('pnpm', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
+execa('pnpm', ['--filter', 'dash', 'ui:check-watch'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
+execa('pnpm', ['--filter', 'site', 'check:watch'], {
+    stderr: process.stderr,
+    stdout: process.stdout,
+});
 execa('pnpm', ['--filter', 'ui', 'storybook'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'ui', 'watch'], { stderr: process.stderr, stdout: process.stdout });
 execa('pnpm', ['--filter', 'i18n', 'watch'], { stderr: process.stderr, stdout: process.stdout });
