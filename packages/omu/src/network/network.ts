bf87e2401cb392791b3d00e254e0e1b5b6f2ad91
am230 <111672334+am230@users.noreply.github.com>
2024-04-29 02:27:01 +0900

    ready処理の実装

--- packages/omu/src/network/network.ts
+++ packages/omu/src/network/network.ts
@@ -1,5 +1,6 @@
 import type { TokenProvider } from '../client/token.js';
-import { AnotherConnection, InvalidOrigin, InvalidPacket, InvalidToken, InvalidVersion, OmuError, PermissionDenied } from '../errors.js';
+import type { OmuError } from '../errors.js';
+import { AnotherConnection, InvalidOrigin, InvalidPacket, InvalidToken, InvalidVersion, PermissionDenied } from '../errors.js';
 import { EventEmitter } from '../event-emitter.js';
 import { IdentifierMap } from '../identifier.js';
 import type { Client } from '../index.js';
@@ -67,6 +68,9 @@ export class Network {
                 throw new error(reason.message);
             }
         });
+        this.addPacketHandler(PACKET_TYPES.READY, () => {
+            this.client.listeners.ready.emit();
+        });
     }
 
     public setConnection(connection: Connection): void {
@@ -121,6 +125,10 @@ export class Network {
         await this.listeners.status.emit('connected');
         await this.listeners.connected.emit();
         this.dispatchTasks();
+        this.send({
+            type: PACKET_TYPES.READY,
+            data: null,
+        });
         await listenPromise;
 
         if (recconect) {
@@ -128,7 +136,7 @@ export class Network {
         }
     }
 
-    private async tryReconnect() {
+    private async tryReconnect(): Promise<void> {
         if (this.closed) {
             return;
         }
