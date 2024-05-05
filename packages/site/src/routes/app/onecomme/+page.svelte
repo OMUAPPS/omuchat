96b1cffdc352c876794bde4db5635380e54854e1
am230 <111672334+am230@users.noreply.github.com>
2024-05-06 01:24:31 +0900

    わんコメテンプレート同期アプリを追加

---
+++ packages/site/src/routes/app/onecomme/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { Header } from '@omuchatjs/ui';
+</script>
+
+<Header title="わんコメテンプレート同期" icon="ti-dog"></Header>
---
+++ packages/site/src/routes/app/onecomme/app.ts
@@ -0,0 +1,24 @@
+import { App } from '@omuchatjs/omu';
+import { Identifier } from '@omuchatjs/omu/identifier.js';
+import type { TagKey } from '../category.js';
+
+export const IDENTIFIER = Identifier.fromKey('cc.omuchat:onecomme');
+
+export default function getApp(origin: string) {
+    return new App(IDENTIFIER, {
+        url: origin + '/app/onecomme',
+        metadata: {
+            locale: 'en',
+            name: {
+                en: 'OneComme Integration',
+                ja: 'わんコメ連携',
+            },
+            description: {
+                en: 'Display comments in the OneComme template without any settings.',
+                ja: '設定無しでコメントをわんコメのテンプレートに表示します。',
+            },
+            icon: 'dog',
+            tags: ['tool'] as TagKey[],
+        }
+    });
+}
