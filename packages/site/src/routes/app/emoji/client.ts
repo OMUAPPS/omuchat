de75d734b08ec4e73fefb980e33555b6e28ffae0
am230 <111672334+am230@users.noreply.github.com>
2024-05-10 14:30:44 +0900

    fix(app-emoji): 絵文字アプリを動くように

--- packages/site/src/routes/app/emoji/client.ts
+++ packages/site/src/routes/app/emoji/client.ts
@@ -1,12 +1,17 @@
 import { Client } from '@omuchatjs/chat';
 import { App } from '@omuchatjs/omu';
+import { ASSET_UPLOAD_MANY_PERMISSION_ID } from '@omuchatjs/omu/extension/asset/asset-extension.js';
 import { setClient } from '@omuchatjs/ui';
 import { IDENTIFIER } from './app.js';
 
 const app = new App(IDENTIFIER, {
     version: '0.1.0',
 });
-export const client = new Client({
+export const client = setClient(new Client({
     app,
-});
-setClient(client);
+}));
+
+client.permissions.require(
+    ASSET_UPLOAD_MANY_PERMISSION_ID,
+);
+
