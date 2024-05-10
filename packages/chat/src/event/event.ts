d98906daf2bc2d87b34ca7b932d253a93178148b
am230 <111672334+am230@users.noreply.github.com>
2024-05-10 12:32:31 +0900

    fix(chat): event.ts

--- packages/chat/src/event/event.ts
+++ packages/chat/src/event/event.ts
@@ -18,7 +18,7 @@ function initTableEvent<T extends Keyable>(consumer: (client: Client) => Table<T
     return (client: Client, invoke: (...data: T[]) => void): void => {
         const table = consumer(client);
         table.listen();
-        table.event.add.subscribe((items) => {
+        table.event.add.listen((items) => {
             for (const item of items.values()) {
                 invoke(item);
             }
@@ -28,7 +28,7 @@ function initTableEvent<T extends Keyable>(consumer: (client: Client) => Table<T
 
 export const events = {
     Ready: new EventKey<[]>('ready', (client, invoke) => {
-        client.network.event.connected.subscribe(() => invoke());
+        client.network.event.connected.listen(() => invoke());
     }),
     MessageCreate: new EventKey<[Message]>(
         'on_message',
