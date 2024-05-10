a906df6e91ada4d3477ac98e84c05332b96d73e0
am230 <111672334+am230@users.noreply.github.com>
2024-05-10 14:26:49 +0900

    fix(app-reaction): リアクションの画像置き換えを動くように

--- packages/site/src/routes/app/reaction/client.ts
+++ packages/site/src/routes/app/reaction/client.ts
@@ -1,4 +1,5 @@
 import { Client } from '@omuchatjs/chat';
+import { ASSET_UPLOAD_PERMISSION_ID } from '@omuchatjs/omu/extension/asset/asset-extension.js';
 import { setClient } from '@omuchatjs/ui';
 import { BROWSER } from 'esm-env';
 import getApp from './app.js';
@@ -7,6 +8,8 @@ export const client = new Client({
     app: getApp(BROWSER ? window.location.origin : ''),
 });
 setClient(client);
+
 client.permissions.require(
+    ASSET_UPLOAD_PERMISSION_ID,
     'cc.omuchat:chatprovider/youtube/reaction',
 );
