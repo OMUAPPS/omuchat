3aa8c298b7447ef520dbe4ac16081d81543065ba
2.30am <111672334+am230@users.noreply.github.com>
2024-05-28 11:52:55 +0900

    enhance(site): footer.svelteのリンクをクリックしやすいように

--- packages/site/src/lib/components/Footer.svelte
+++ packages/site/src/lib/components/Footer.svelte
@@ -6,21 +6,27 @@
     <FlexRowWrapper gap between heightFull>
         <FlexColWrapper gap>
             <p>Links</p>
-            <p>
-                <i class="ti ti-brand-github" />
-                <a href="https://github.com/OMUAPPS/omuapps">Github</a>
-                <i class="ti ti-external-link" />
-            </p>
-            <p>
-                <i class="ti ti-brand-twitter" />
-                <a href="https://twitter.com/omuapps">Twitter</a>
-                <i class="ti ti-external-link" />
-            </p>
-            <p>
-                <i class="ti ti-brand-discord" />
-                <a href="https://discord.gg/MZKvbPpsuK">Discord</a>
-                <i class="ti ti-external-link" />
-            </p>
+            <a href="https://github.com/OMUAPPS/omuapps">
+                <p>
+                    <i class="ti ti-brand-github" />
+                    Github
+                    <i class="ti ti-external-link" />
+                </p>
+            </a>
+            <a href="https://twitter.com/omuapps">
+                <p>
+                    <i class="ti ti-brand-twitter" />
+                    Twitter
+                    <i class="ti ti-external-link" />
+                </p>
+            </a>
+            <a href="https://discord.gg/MZKvbPpsuK">
+                <p>
+                    <i class="ti ti-brand-discord" />
+                    Discord
+                    <i class="ti ti-external-link" />
+                </p>
+            </a>
         </FlexColWrapper>
         <p style="text-align: end; margin-top: auto;">
             <a href="https://x.com/2ji_han">🕝</a>
@@ -38,7 +44,7 @@
         padding: 2rem 0;
     }
 
-    a {
+    p {
         font-weight: 200;
     }
 </style>
