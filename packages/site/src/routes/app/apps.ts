e97f0440f0b7672e480fc13332439368200b12f9
2.30am <111672334+am230@users.noreply.github.com>
2024-06-02 17:04:29 +0900

    fix(site): 開発時のアプリのアイコンurlにoriginが含まれるように

--- packages/site/src/routes/app/apps.ts
+++ packages/site/src/routes/app/apps.ts
@@ -1,30 +1,42 @@
 import { App } from '@omujs/omu';
+import { BROWSER, DEV } from 'esm-env';
 import { omu } from '../client.js';
+import { APP as archive } from './archive/app.js';
+import { APP as caption } from './caption/app.js';
 import { APP as emoji } from './emoji/app.js';
+import { APP as lipsynctest } from './lipsynctest/app.js';
 import { APP as notifier } from './notifier/app.js';
 import { APP as onecomme } from './onecomme/app.js';
 import { APP as playqueue } from './playqueue/app.js';
 import { APP as reaction } from './reaction/app.js';
+import { APP as replay } from './replay/app.js';
 import { APP as tester } from './tester/app.js';
 import { APP as translator } from './translator/app.js';
-import { APP as caption } from './caption/app.js';
-import { APP as archive } from './archive/app.js';
-import { APP as replay } from './replay/app.js';
-import { APP as lipsynctest } from './lipsynctest/app.js';
 
 export const apps = [
+    archive,
+    caption,
     emoji,
+    lipsynctest,
     notifier,
     onecomme,
     playqueue,
     reaction,
+    replay,
     tester,
     translator,
-    caption,
-    archive,
-    replay,
-    lipsynctest,
 ] satisfies App[];
+if (DEV && BROWSER) {
+    apps.forEach((app) => {
+        const origin = 'http://localhost:5173';
+        if (!app.metadata) return;
+        const icon = app.metadata?.icon;
+        if (typeof icon !== 'string') return;
+        if (icon.startsWith('ti-')) return;
+        const url = new URL(icon, origin);
+        app.metadata.icon = url.href;
+    });
+}
 
 export const appTable = omu.dashboard.apps;
 omu.onReady(async () => {
