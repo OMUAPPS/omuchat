a1e16567fd6adcfd4afa29bd369e8b915c436d47
2.30am <111672334+am230@users.noreply.github.com>
2024-06-14 19:07:10 +0900

    feat(site): 個人用アプリ機能

--- packages-js/site/src/routes/app/apps.ts
+++ packages-js/site/src/routes/app/apps.ts
@@ -26,6 +26,17 @@ export const apps = [
     translator,
     chatSubtitle,
 ] satisfies App[];
+
+const personalApps: Record<string, App[]> = {
+};
+
+if (BROWSER) {
+    const pass = BROWSER && new URL(window.location.href).searchParams.get('pass');
+    if (pass && pass in personalApps) {
+        apps.unshift(...personalApps[pass])
+    }
+}
+
 if (DEV && BROWSER) {
     apps.forEach((app) => {
         const origin = 'http://localhost:5173';
