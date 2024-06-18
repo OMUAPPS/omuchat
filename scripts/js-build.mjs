81ce320dfd087b18dfd7896b07abc714668d267b
2.30am <111672334+am230@users.noreply.github.com>
2024-06-18 16:12:08 +0900

    bunで動くように

--- scripts/js-build.mjs
+++ scripts/js-build.mjs
@@ -1,20 +1,20 @@
 import { execa, execaSync } from 'execa';
 
 await Promise.all([
-    execa('pnpm', ['--filter', 'ui', 'build'], {
+    execa('bun', ['--filter', '@omujs/ui', 'build'], {
         stderr: process.stderr,
         stdout: process.stdout,
     }),
-    execa('pnpm', ['--filter', 'i18n', 'build'], {
+    execa('bun', ['--filter', '@omujs/i18n', 'build'], {
         stderr: process.stderr,
         stdout: process.stdout,
     }),
-    execa('pnpm', ['--filter', 'omu', 'build'], {
+    execa('bun', ['--filter', '@omujs/omu', 'build'], {
         stderr: process.stderr,
         stdout: process.stdout,
     }),
 ]);
-execaSync('pnpm', ['--filter', 'chat', 'build'], {
+execaSync('bun', ['--filter', '@omujs/chat', 'build'], {
     stderr: process.stderr,
     stdout: process.stdout,
 });
@@ -22,7 +22,7 @@ execaSync('pnpm', ['--filter', 'chat', 'build'], {
 if (process.argv.includes('--build')) {
     const targets = process.argv[process.argv.indexOf('--build') + 1];
     targets.split(',').forEach((target) => {
-        execaSync('pnpm', ['--filter', target, 'build'], {
+        execaSync('bun', ['--filter', target, 'build'], {
             stderr: process.stderr,
             stdout: process.stdout,
         });
--- scripts/js-dev.mjs
+++ scripts/js-dev.mjs
@@ -1,17 +1,17 @@
 import { execa } from 'execa';
 import './js-build.mjs';
 
-execa('pnpm', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'dash', 'ui:check-watch'], {
+execa('bun', ['--filter', 'dash', 'dev'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', 'site', 'dev'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', 'dash', 'ui:check-watch'], {
     stderr: process.stderr,
     stdout: process.stdout,
 });
-execa('pnpm', ['--filter', 'site', 'check:watch'], {
+execa('bun', ['--filter', 'site', 'check:watch'], {
     stderr: process.stderr,
     stdout: process.stdout,
 });
-execa('pnpm', ['--filter', 'ui', 'watch'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'i18n', 'watch'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'omu', 'watch'], { stderr: process.stderr, stdout: process.stdout });
-execa('pnpm', ['--filter', 'chat', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', '@omujs/ui', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', '@omujs/i18n', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', '@omujs/omu', 'watch'], { stderr: process.stderr, stdout: process.stdout });
+execa('bun', ['--filter', '@omujs/chat', 'watch'], { stderr: process.stderr, stdout: process.stdout });
